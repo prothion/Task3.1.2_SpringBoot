5e58cb6c7e45bc510d83936246ab6e66713090f5
prothion <derechinvd@gmail.com>
2022-12-17 11:57:00 +0300

    Убрал JpaRepository

--- src/main/java/com/example/task311springboot/repository/UsersRepository.java
+++ src/main/java/com/example/task311springboot/repository/UsersRepository.java
@@ -1,10 +1,9 @@
 package com.example.task311springboot.repository;
 
 import com.example.task311springboot.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UsersRepository extends JpaRepository<User, Long> {
+public interface UsersRepository {
 }
 
